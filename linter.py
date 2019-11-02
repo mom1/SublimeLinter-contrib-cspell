5e68f4783d76f5109f9cec178f177c81dd722495
absszero <absszero@gmail.com>
2019-11-02 13:05:53 +0800

    use stdin

--- linter.py
+++ linter.py
@@ -1,9 +1,7 @@
-from SublimeLinter.lint import Linter, STREAM_STDOUT
+from SublimeLinter.lint import Linter
 
 
 class CSpell(Linter):
-    cmd = 'cspell ${file_on_disk}'
+    cmd = 'cspell --no-color stdin'
     defaults = {'selector': 'source'}
-    regex = r'^(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
-    tempfile_suffix = '-'
-    error_stream = STREAM_STDOUT
+    regex = r'(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)'
