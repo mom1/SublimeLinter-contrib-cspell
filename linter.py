fc12cec9822dc320e0eab9b4c61f94dadbe8d693
absszero <absszero@gmail.com>
2019-11-04 20:40:01 +0800

    show as warning

--- linter.py
+++ linter.py
@@ -1,7 +1,9 @@
 from SublimeLinter.lint import Linter
+from SublimeLinter.lint import const
 
 
 class CSpell(Linter):
-    cmd = 'cspell --no-color stdin'
+    default_type = const.WARNING
+    cmd = 'cspell --no-color --no-summary stdin'
     defaults = {'selector': 'source'}
     regex = r'(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)'
