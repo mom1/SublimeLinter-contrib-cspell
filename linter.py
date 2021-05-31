2f593333ed559023c8c3ded83241e2e4abdba64b
MaxST <mstolpasov@gmail.com>
2021-05-31 14:55:36 +0300

    Quick Actions
    - `Add word "{word}" to dictionary`  - add word to `cspell.json`
    - `Disable line with word` - add `# cspell:disable-line` to end of line

--- linter.py
+++ linter.py
@@ -1,5 +1,10 @@
-from SublimeLinter.lint import Linter
-from SublimeLinter.lint import const
+import json
+from functools import partial
+from pathlib import Path
+
+from SublimeLinter.lint import Linter, const
+from SublimeLinter.lint.quick_fix import (QuickAction, add_at_eol,
+                                          line_error_is_on, quick_actions_for)
 
 
 class CSpell(Linter):
@@ -7,3 +12,48 @@ class CSpell(Linter):
     cmd = ('cspell', '--no-color', '--no-summary', '${args}', 'stdin')
     defaults = {'selector': 'source'}
     regex = r'(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)'
+
+
+@quick_actions_for('cspell')
+def cspell_fix(errors, view):
+    for error in errors:
+        yield QuickAction(
+            '{linter}: Add word "{offending_text}" to dictionary'.format(
+                **error),
+            partial(fix_cspell_error, error),
+            '',
+            solves=error,
+        )
+        yield QuickAction(
+            '{linter}: Disable line with word "{offending_text}"'.format(
+                **error),
+            partial(fix_disable_cspell_error, error),
+            '',
+            solves=error,
+        )
+
+
+def fix_cspell_error(error, view):
+    word = error.get('offending_text')
+    if word:
+        path = Path(next(iter(view.window().folders()), None), 'cspell.json')
+        cspell = json.load(path.open('r', encoding='utf8'))
+        words = set(cspell['words'])
+        words.add(word.lower())
+        cspell['words'] = sorted(words)
+        json.dump(
+            cspell,
+            path.open('w', encoding='utf8'),
+            ensure_ascii=False,
+            indent=2,
+        )
+    yield line_error_is_on(view, error)
+
+
+def fix_disable_cspell_error(error, view):
+    line = line_error_is_on(view, error)
+    yield add_at_eol(
+        ('  # cspell:disable-line'
+         if '#' not in line.text.rstrip() else ' cspell:disable-line'),
+        line,
+    )
