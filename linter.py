dae7bd49480e5da7fc110eeb88d314abab84996b
MaxST <mstolpasov@gmail.com>
2021-05-31 11:25:01 +0300

    set custom args in cmd

--- linter.py
+++ linter.py
@@ -4,6 +4,6 @@ from SublimeLinter.lint import const
 
 class CSpell(Linter):
     default_type = const.WARNING
-    cmd = 'cspell --no-color --no-summary stdin'
+    cmd = ('cspell', '--no-color', '--no-summary', '${args}', 'stdin')
     defaults = {'selector': 'source'}
     regex = r'(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)'
