984885644381096cd4572974af6148cd51731416
MaxST <mstolpasov@gmail.com>
2022-02-09 18:47:49 +0300

    Update linter.py

--- linter.py
+++ linter.py
@@ -11,7 +11,7 @@ class CSpell(Linter):
     default_type = const.WARNING
     cmd = ('cspell', '--no-color', '--no-summary', '${args}', 'stdin')
     defaults = {'selector': 'source'}
-    regex = r'(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)'
+    regex = r'([^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)'
 
 
 @quick_actions_for('cspell')
